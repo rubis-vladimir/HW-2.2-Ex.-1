af22931d49cbfa5a97b9616957b9c8cf10899388
rubis-vladimir <rubis-vladimir>
2021-05-21 14:02:54 +0300

    Первая рабочая версия

--- shadesOfColor/ViewController.swift
+++ shadesOfColor/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var viewForColorMix: BorderView!
+    @IBOutlet var saturatedOfColor: [UILabel]!
+    @IBOutlet weak var sliderRed: UISlider!
+    @IBOutlet weak var sliderGreen: UISlider!
+    @IBOutlet weak var sliderBlue: UISlider!
+    @IBOutlet var sliders: [UISlider]!
+    @IBOutlet weak var sliderAlpha: UISlider! {
+        didSet {
+            sliderAlpha.transform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi / 2))
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewForColorMix.backgroundColor = UIColor(red: 0.75, green: 0.5, blue: 0.25, alpha: 1)
+    }
+    
+    func collectColor () {
+        viewForColorMix.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: CGFloat(sliderAlpha.value))
     }
 
-
+    @IBAction func rgbaSliderChanged(_ sender: UISlider) {
+        collectColor()
+        for index in 0..<saturatedOfColor.count {
+            saturatedOfColor[index].text = String(round(Double(sliders[index].value), toDecimalPlaces: 2))
+        }
+    }
 }
 
+func round (_ value: Double, toDecimalPlaces places: Int) -> Double {
+    let divisor = pow(10.0, Double(places))
+    return round (value * divisor) / divisor
+}
+
+@IBDesignable class BorderView : UIView {
+    @IBInspectable var borderColor: UIColor = .clear {
+        didSet {
+        layer.borderColor = borderColor.cgColor
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+}
